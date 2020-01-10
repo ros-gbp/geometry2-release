59ad9aeb325968cfc1948e4501a67d4068067fa8
Tully Foote <tfoote@osrfoundation.org>
2020-01-09 17:02:32 -0800

    Rebase from 'upstream'

--- tf2_ros/src/buffer_server_main.cpp
+++ tf2_ros/src/buffer_server_main.cpp
@@ -38,20 +38,6 @@
 #include <tf2_ros/transform_listener.h>
 #include <ros/ros.h>
 
-
-class MyClass
-{
-public:
-  MyClass() {}
-  MyClass(double d) {}
-};
-
-class BlankClass
-{
-public:
-  BlankClass() {}
-};
-
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "tf_buffer");
@@ -59,6 +45,9 @@ int main(int argc, char** argv)
 
   double buffer_size;
   nh.param("buffer_size", buffer_size, 120.0);
+
+  bool publish_frame_service;
+  nh.param("publish_frame_service", publish_frame_service, false);
 
   // Legacy behavior re: #209
   bool use_node_namespace;
@@ -73,13 +62,10 @@ int main(int argc, char** argv)
     node_name = "tf2_buffer_server";
   }
 
-  // WIM: this works fine:
-  tf2_ros::Buffer buffer_core(ros::Duration(buffer_size+0)); // WTF??
+  tf2_ros::Buffer buffer_core(ros::Duration(buffer_size), publish_frame_service);
   tf2_ros::TransformListener listener(buffer_core);
   tf2_ros::BufferServer buffer_server(buffer_core, node_name , false);
   buffer_server.start();
-  // But you should probably read this instead:
-  // http://www.informit.com/guides/content.aspx?g=cplusplus&seqNum=439
 
   ros::spin();
 }
--- tf2_sensor_msgs/CHANGELOG.rst
+++ tf2_sensor_msgs/CHANGELOG.rst
@@ -2,6 +2,12 @@
 Changelog for package tf2_sensor_msgs
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
+0.6.6 (2020-01-09)
+------------------
+* Affine->Isometry `#378 <https://github.com/ros/geometry2/issues/378>`_
+* Python 3 compatibility: relative imports and print statement
+* Contributors: Martin Pecka, Timon Engelke, Tully Foote
+
 0.6.5 (2018-11-16)
 ------------------
 
